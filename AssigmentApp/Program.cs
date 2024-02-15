a5d2f4d5509d35d260c49cd898d6113f4bdfdf00
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-15 14:40:29 +0530

    Add project files.

---
+++ AssigmentApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Even And Odd Numbers");
+for  (int i = 1; i < 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("{0} IS EVEN NUMBER ", i);
+
+    }
+    else { Console.WriteLine("{0} is ODD NUMBER", i);
+    }
+
+   
+}
